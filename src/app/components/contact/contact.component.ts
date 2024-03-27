f30105324f7e2ea8d6009773a61bf6d2ed3dde62
Luis Reyes <luisandresreyes@hotmail.com>
2024-03-27 14:19:02 -0300

    Fin desafio 3 formularios reactivos

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -4,29 +4,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  formularioContacto: FormGroup;
+  nombreCorrecto? = true;
+  apellidoCorrecto? = true;
+  telefonoCorrecto? = true;
+  emailCorrecto? = true;
 
-  formularioContacto: FormGroup
-  submitted = false;
-
-  constructor( private form: FormBuilder) {
-    this.formularioContacto = this.form.group ({
+  constructor(private form: FormBuilder) {
+    this.formularioContacto = this.form.group({
       nombre: ['', [Validators.required, Validators.minLength(3)]],
       apellido: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
-      telefono: ['', [Validators.required, Validators.pattern("[0-9]{13}")]]
-    })
+      telefono: ['', [Validators.required, Validators.pattern('[0-9]{13}')]],
+    });
   }
 
-  hasError( controlName: string, errorType: string) {
-    return this.formularioContacto.get(controlName)?.hasError(errorType) && this.formularioContacto.get(controlName)?.touched
+  hasError(controlName: string, errorType: string) {
+    const control = this.formularioContacto.get(controlName);
+    return control?.touched && control?.hasError(errorType);
   }
 
   enviar() {
-    console.log(this.formularioContacto);
+    if (this.formularioContacto.valid) {
+      console.log('Envío exitoso');
+    } else {
+      console.log('Formulario inválido');
+      this.formularioContacto.markAllAsTouched();
+    }
   }
-
-  
 }
